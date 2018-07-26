de68cacff777d5c977051238bbeb66a344779318
wambui7 <shiko.sheila@gmail.com>
2018-07-26 16:51:48 -0500

    Add project files.

---
+++ number guess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace number_guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
