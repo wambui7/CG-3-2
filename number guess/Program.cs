c5128b809a95c6c8c9025a0d09f940080d94258b
wambui7 <shiko.sheila@gmail.com>
2018-07-26 17:02:04 -0500

    commit changes

--- number guess/Program.cs
+++ number guess/Program.cs
@@ -6,7 +6,12 @@ namespace number_guess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("make a guess of number between 1 -20");
+            string guesser = Console.ReadLine();
+
+            string winloose = (guesser == "8") ? "win" : "loose";
+            Console.WriteLine(guesser + winloose + ".");
+            Console.ReadLine();
         }
     }
 }
